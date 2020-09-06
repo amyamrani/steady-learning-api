bd8c31e92ba1e81b840521b08c5a392239e03ac6
Amy Amrani <vesamrani@gmail.com>
2020-09-06 00:22:24 -0400

    added db migration scripts and seeds

---
+++ seeds/seed.articles.sql
@@ -0,0 +1,32 @@
+INSERT INTO articles (title, topic, url)
+VALUES
+  (
+    'How to Make a Cake',
+    'Cooking Basics',
+    'https://www.bhg.com/recipes/how-to/bake/how-to-make-a-cake/'
+  ),
+  (
+    'Grill a Steak',
+    'Cooking Basics',
+    'https://www.foodnetwork.com/how-to/articles/how-to-grill-steak-a-step-by-step-guide'
+  ),
+  (
+    'Scramble an Egg',
+    'Cooking Basics',
+    'https://www.thespruceeats.com/perfect-scrambled-eggs-995518'
+  ),
+  (
+    'Prepping Your Paint Jobs Like a Pro',
+    'Basic Home Repairs',
+    'https://home.howstuffworks.com/home-improvement/repair/5-home-repairs-you-should-do-yourself.htm#pt1'
+  ),
+  (
+    'Dealing with Doors',
+    'Basic Home Repairs',
+    'https://home.howstuffworks.com/home-improvement/repair/5-home-repairs-you-should-do-yourself.htm#pt2'
+  ),
+  (
+    'Applying Caulk',
+    'Basic Home Repairs',
+    'https://home.howstuffworks.com/home-improvement/repair/5-home-repairs-you-should-do-yourself.htm#pt3'
+  );
