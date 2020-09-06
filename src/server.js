3a2889523392d9b6464e441293d2f9ae786069e8
Amy Amrani <vesamrani@gmail.com>
2020-09-05 22:29:46 -0400

    testing deployment

--- src/server.js
+++ src/server.js
@@ -1,7 +1,21 @@
-const app = require('./app')
+const express = require('express');
+const app = express();
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = {app};
+
+
+// const app = require('./app')
+
+// const PORT = process.env.PORT || 8000
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening at http://localhost:${PORT}`)
+// })
