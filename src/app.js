765e2e42abf4198153f299a5ceee909a1c78ee37
Amy Amrani <vesamrani@gmail.com>
2020-09-07 22:33:05 -0400

    added authentication, logger and error handling

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const {CLIENT_ORIGIN} = require('./config');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
+const errorHandler = require('./errorHandler')
+const validateBearerToken = require('./validateBearerToken')
 
 const app = express()
 
@@ -20,19 +21,12 @@ app.use(
   })
 );
 
+app.use(validateBearerToken)
+
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
-    console.error(error)
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response)
- })
+app.use(errorHandler)
 
 module.exports = app
