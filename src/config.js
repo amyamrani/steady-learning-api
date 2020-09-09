6da3dd14ac77d8ee315c7c148bf912285117b5b5
Amy Amrani <vesamrani@gmail.com>
2020-09-09 12:54:49 -0400

    updated database url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://amy@localhost/steady_learning',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://amy@localhost/steady-learning',
 }
